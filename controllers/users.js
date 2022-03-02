87baab81f1e3e85fe8633cb85226681bd13f4382
Antuanett-Lephebvre <72161650+Antuanett-Lephebvre@users.noreply.github.com>
2022-03-02 16:23:43 +0300

    Create users.js

---
+++ controllers/users.js
@@ -0,0 +1,81 @@
+const User = require('../models/user');
+
+const getUsers = (req, res) => {
+  User.find({})
+    .then((users) => res.status(200).send({ data: users }))
+    .catch((err) => {
+      res.status(500).send({ message: `На сервере произошла ошибка: ${err}` });
+    });
+};
+
+const getUser = (req, res) => {
+  User.findById(req.params.id)
+    .then((user) => {
+      if (!user) {
+        return res.status(404).send({ message: 'Нет пользователя с таким id' });
+      }
+      return res.status(200).send({ data: user });
+    })
+    .catch((err) => {
+      if (err.name === 'CastError') {
+        res.status(400).send({ message: 'Невалидный id ' });
+      } else {
+        res.status(500).send({ message: `На сервере произошла ошибка: ${err}` });
+      }
+    });
+};
+
+const createUser = (req, res) => {
+  const { name, about, avatar } = req.body;
+  User.create({ name, about, avatar })
+    .then((user) => res.status(200).send({ data: user }))
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        res.status(400).send({ message: `Переданы некорректные данные: ${err}` });
+      } else {
+        res.status(500).send({ message: `На сервере произошла ошибка: ${err}` });
+      }
+    });
+};
+
+const updateProfile = (req, res) => {
+  const { name, about } = req.body;
+  User.findByIdAndUpdate(
+    req.user._id,
+    { name, about },
+    { new: true, runValidators: true },
+  )
+    .then((user) => res.status(202).send({ data: user }))
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        res.status(400).send({ message: 'Переданы некорректные данные в методы обновления аватара' });
+      } else {
+        res.status(500).send({ message: `На сервере произошла ошибка: ${err}` });
+      }
+    });
+};
+
+const updateAvatar = (req, res) => {
+  const { avatar } = req.body;
+  User.findByIdAndUpdate(
+    req.user._id,
+    { avatar },
+    { new: true, runValidations: true },
+  )
+    .then((user) => res.status(200).send({ data: user }))
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        res.status(400).send({ mesage: 'Переданы некорректные данные в методы обновления аватара' });
+      } else {
+        res.status(500).send({ message: `На сервере произошла ошибка: ${err}` });
+      }
+    });
+};
+
+module.exports = {
+  getUsers,
+  getUser,
+  createUser,
+  updateProfile,
+  updateAvatar,
+};
