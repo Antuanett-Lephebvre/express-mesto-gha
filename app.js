f7088b59d1b358cf3d118870621ce1568d8dc82d
Antuanett-Lephebvre <72161650+Antuanett-Lephebvre@users.noreply.github.com>
2022-03-03 22:04:42 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser');
+const { errors } = require('celebrate');
 const userRouter = require('./routes/users');
 const cardRouter = require('./routes/cards');
 const { createUser, login } = require('./controllers/users');
@@ -13,26 +15,26 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
 });
 
+app.use(express.json());
+app.use(cookieParser());
+
+app.post('/signin', login);
+app.post('/signup', createUser);
+
+app.use('/', auth, userRouter);
+app.use('/', auth, cardRouter);
+
+app.use(errors());
 app.use((err, req, res, next) => {
   const { statusCode = 500, message } = err;
   res.status(statusCode).send({
     message: statusCode === 500 ? 'На сервере произошла ошибка' : message,
   });
   next();
-});
-
-app.use(express.json());
-
-app.use('/', auth, userRouter);
-app.use('/', auth, cardRouter);
+}, auth);
 
 app.use(() => {
   throw new NotFound('Несуществующий роут');
 });
-
-app.post('/signin', login);
-app.post('/signup', createUser);
-
 app.listen(PORT, () => {
-  console.log(PORT);
 });
