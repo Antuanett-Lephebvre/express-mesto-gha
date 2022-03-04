e65f89171655f4e25d6711fb306cdc209f5aedbd
Antuanett-Lephebvre <72161650+Antuanett-Lephebvre@users.noreply.github.com>
2022-03-04 16:13:25 +0300

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,5 @@
 const router = require('express').Router();
 
-module.exports = router;
 const {
   getUsers,
   getUser,
@@ -8,11 +7,16 @@ const {
   updateAvatar,
   getCurrentUser,
 } = require('../controllers/users');
+const {
+  validationUpdateAvatar,
+  validationUpdateProfile,
+  validationUserId,
+} = require('../middlewares/validation');
 
-router.get('/', getUsers);
-router.get('/:userId', getUser);
-router.patch('/me', updateProfile);
-router.get('/me', getCurrentUser);
-router.patch('/me/avatar', updateAvatar);
+router.get('/users', getUsers);
+router.get('/users/me', getCurrentUser);
+router.get('/users/:id', validationUserId, getUser);
+router.patch('/users/me', validationUpdateProfile, updateProfile);
+router.patch('/users/me/avatar', validationUpdateAvatar, updateAvatar);
 
 module.exports = router;
