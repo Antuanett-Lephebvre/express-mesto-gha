8f2ce2baa8ce065610673266ef1afa7d6773977e
Antuanett-Lephebvre <72161650+Antuanett-Lephebvre@users.noreply.github.com>
2022-03-04 16:13:53 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
+
 const { errors } = require('celebrate');
 const userRouter = require('./routes/users');
 const cardRouter = require('./routes/cards');
 const { createUser, login } = require('./controllers/users');
 const auth = require('./middlewares/auth');
+const {
+  validationLogin,
+  validationCreateUser,
+} = require('./middlewares/validation');
 const NotFound = require('./errors/NotFound');
 
 const PORT = 3000;
@@ -18,11 +23,15 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
 app.use(express.json());
 app.use(cookieParser());
 
-app.post('/signin', login);
-app.post('/signup', createUser);
+app.post('/signin', validationLogin, login);
+app.post('/signup', validationCreateUser, createUser);
 
 app.use('/', auth, userRouter);
 app.use('/', auth, cardRouter);
+
+app.use(auth, () => {
+  throw new NotFound('Несуществующий роут');
+});
 
 app.use(errors());
 app.use((err, req, res, next) => {
@@ -31,10 +40,7 @@ app.use((err, req, res, next) => {
     message: statusCode === 500 ? 'На сервере произошла ошибка' : message,
   });
   next();
-}, auth);
-
-app.use(() => {
-  throw new NotFound('Несуществующий роут');
 });
+
 app.listen(PORT, () => {
 });
